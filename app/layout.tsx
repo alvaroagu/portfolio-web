6da5a62690c4691d3d05fcb8ac1bbdaee866e57e
alvaroagu <alvaro.aguinagalde@gmail.com>
2025-06-12 23:10:22 -0400

    fixed: problema de render con hydration

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+    <html lang="en">
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
