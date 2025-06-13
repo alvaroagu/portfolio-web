bcbae77b22cbb080b3ae977a01248180b09c0759
alvaroagu <alvaro.aguinagalde@gmail.com>
2025-06-12 23:09:36 -0400

    Componentes para navbar

---
+++ app/layout/ContentLayout.tsx
@@ -0,0 +1,19 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+interface ContentLayoutProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export function ContentLayout({ title, children }: ContentLayoutProps) {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <div className="container pt-8 pb-8 px-4 sm:px-8]">{children}</div>
+        <Footer />
+      </div>
+    </>
+  );
+}
